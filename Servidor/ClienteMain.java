5eae5f84bb4d953b5a40e9e04ef9f4e75a759c33
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2022-10-07 17:48:27 -0300

    Esta dando erro, mas nao estou conseguindo rodar a aplicacoa no mesmo terminal, nem tampouco abrir em separados.

--- Servidor/ClienteMain.java
+++ Servidor/ClienteMain.java
@@ -1,7 +1,24 @@
 package Servidor;
 
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
 public class ClienteMain {
 	public static void main(String[] args) {
-		System.out.println("Olá");
+		System.out.println("Olá sou o cliente, vamos sortear uma frase.");
+		try {
+			Socket conxServidor= new Socket("Pedro",12345); // abre uma conexão na porta 12345
+			// abre uma conexão de entrada com o servidor que receberá um stream de bits
+			ObjectInputStream entrada = new ObjectInputStream(conxServidor.getInputStream());
+			String mensagemRecebida=(String)entrada.readObject();
+			System.out.println(mensagemRecebida);
+			entrada.close();
+			System.out.println("Conexão encerrada");
+			
+			
+		}catch(Exception e) {
+			System.out.println("Erro: "+e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
